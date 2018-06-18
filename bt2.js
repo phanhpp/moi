8ef3d61e09c8a63a8c97b7be0c4d8c6ad428904d
Đặng Phương Anh <phuongadang@gmail.com>
2018-06-18 12:09:27 +0700

    udate file bt2.js

--- bt2.js
+++ bt2.js
@@ -1,2 +1,25 @@
-var a =0;
-var b = 0;
+//Bai 1
+var s =0;
+const tinhTong = (n) => {
+    
+ for (i = 1; i <= n; i++){
+     s+= i^2;
+ }
+ console.log(s);
+}
+tinhTong(9)
+
+//bai3:
+ tich = 1;
+function tichChinh(N) {
+    for ( i = 1; i <= N; i++){
+    d= Math.sqrt(i);
+    if ( parseInt(d)== d){ 
+        console.log(i);
+        tich *= i;
+
+    }
+} console.log(tich);
+}
+tichChinh(26)
+
